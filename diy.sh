e04968232c8f41073130a7d3b15ab6d4ee7ad21d
Mike Chen <517926804@qq.com>
2020-08-12 23:27:32 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,7 +12,7 @@ sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_gener
 sed -i 's/OpenWrt/RaspberryPi4/g' package/base-files/files/bin/config_generate
 
 #修复核心及添加温度显示
-sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+#sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
 cp -rf ../.rpi-4 .config
